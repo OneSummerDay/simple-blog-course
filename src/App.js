1c50adc0aedbb700e2b4ba03f68b0b10e02e2878
OneSummerDay <artem.kraievyi@gmail.com>
2023-08-07 16:47:17 +0300

    feature: create App.css file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
 
 function App() {
     //const [likes, setLikes] = useState(0, )
 
     return (
         <div className="App">
-            <ClassCounter />
+            <div className="post">
+                <div className="post__content">
+                    <strong>1. First Post</strong>
+                    <div>JavaScript si a popular!</div>
+                </div>
+                <div className="post__btn">
+                    <button>Delete</button>
+                </div>
+
+            </div>
         </div>
     );
 }
